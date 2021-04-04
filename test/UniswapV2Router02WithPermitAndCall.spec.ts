e8ad2998792836b3933f5e1c03acfbefab199958
sam <sam@freighttrust.com>
2021-04-03 21:44:54 -0700

    lint(format): fix
    Signed-off-by: sam <sam@freighttrust.com>

--- test/UniswapV2Router02WithPermitAndCall.spec.ts
+++ test/UniswapV2Router02WithPermitAndCall.spec.ts
@@ -1,37 +1,47 @@
-import { use } from 'chai'
-import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle'
-import { Contract, BigNumber, constants, utils, ContractInterface } from 'ethers'
+import { use } from 'chai';
+import { solidity, MockProvider, createFixtureLoader } from 'ethereum-waffle';
+import {
+  Contract,
+  BigNumber,
+  constants,
+  utils,
+  ContractInterface,
+} from 'ethers';
 
-import { fixture } from './shared/fixture'
-import { GAS_LIMIT, expandTo18Decimals, getPermitSignature } from './shared/utils'
+import { fixture } from './shared/fixture';
+import {
+  GAS_LIMIT,
+  expandTo18Decimals,
+  getPermitSignature,
+} from './shared/utils';
 
-use(solidity)
+use(solidity);
 
 describe('PermitAndCall', () => {
   const provider = new MockProvider({
     ganacheOptions: {
       gasLimit: GAS_LIMIT.toString(),
     },
-  })
-  const [wallet] = provider.getWallets()
-  const loadFixture = createFixtureLoader([wallet], provider)
+  });
+  const [wallet] = provider.getWallets();
+  const loadFixture = createFixtureLoader([wallet], provider);
 
-  let token: Contract
-  let WETH: Contract
-  let router: Contract
-  let permitAndCall: Contract
+  let token: Contract;
+  let WETH: Contract;
+  let router: Contract;
+  let permitAndCall: Contract;
   beforeEach(async function () {
-    const loadedFixture = await loadFixture(fixture)
-    token = loadedFixture.token
-    WETH = loadedFixture.WETH
-    router = loadedFixture.router
-    permitAndCall = loadedFixture.permitAndCall
+    const loadedFixture = await loadFixture(fixture);
+    token = loadedFixture.token;
+    WETH = loadedFixture.WETH;
+    router = loadedFixture.router;
+    permitAndCall = loadedFixture.permitAndCall;
 
     // add liquidity
-    const liquidityAmount = expandTo18Decimals(50)
-    await token.approve(router.address, liquidityAmount)
-    await WETH.deposit({ value: liquidityAmount })
-    await WETH.approve(router.address, liquidityAmount)
+    const liquidityAmount = expandTo18Decimals(50);
+    await token.approve(router.address, liquidityAmount);
+    await WETH.deposit({ value: liquidityAmount });
+    await WETH.approve(router.address, liquidityAmount);
     await router.addLiquidity(
       token.address,
       WETH.address,
@@ -40,32 +50,41 @@ describe('PermitAndCall', () => {
       constants.Zero,
       constants.Zero,
       wallet.address,
-      constants.MaxUint256
-    )
-  })
+      constants.MaxUint256,
+    );
+  });
 
-  const swapAmount = expandTo18Decimals(1)
+  const swapAmount = expandTo18Decimals(1);
 
   it('works if allowance has been granted', async () => {
-    const permitSelector = utils.hexDataSlice(constants.HashZero, 0, 4)
-    const permitData = '0x'
+    const permitSelector = utils.hexDataSlice(constants.HashZero, 0, 4);
+    const permitData = '0x';
 
-    const routerFunctionName = 'swapExactTokensForTokens'
-    const routerFunctionFragment = router.interface.fragments.filter(({ name }) => name === routerFunctionName)[0]
+    const routerFunctionName = 'swapExactTokensForTokens';
+    const routerFunctionFragment = router.interface.fragments.filter(
+      ({ name }) => name === routerFunctionName,
+    )[0];
     const routerFunctionSelector = utils.hexDataSlice(
-      utils.id(`${routerFunctionName}(${routerFunctionFragment.inputs.map(({ type }) => type).join(',')})`),
+      utils.id(
+        `${routerFunctionName}(${routerFunctionFragment.inputs
+          .map(({ type }) => type)
+          .join(',')})`,
+      ),
       0,
-      4
-    )
-    const routerFunctionData = utils.defaultAbiCoder.encode(routerFunctionFragment.inputs, [
-      swapAmount,
-      constants.Zero,
-      [token.address, WETH.address],
-      wallet.address,
-      constants.MaxUint256,
-    ])
+      4,
+    );
+    const routerFunctionData = utils.defaultAbiCoder.encode(
+      routerFunctionFragment.inputs,
+      [
+        swapAmount,
+        constants.Zero,
+        [token.address, WETH.address],
+        wallet.address,
+        constants.MaxUint256,
+      ],
+    );
 
-    await token.approve(permitAndCall.address, swapAmount)
+    await token.approve(permitAndCall.address, swapAmount);
     await permitAndCall.permitAndCall(
       token.address,
       swapAmount,
@@ -73,19 +92,31 @@ describe('PermitAndCall', () => {
       permitData,
       routerFunctionSelector,
       routerFunctionData,
-      { gasLimit: GAS_LIMIT }
-    )
-  })
+      { gasLimit: GAS_LIMIT },
+    );
+  });
 
   it('works if allowance has not been granted', async () => {
-    const permitName = 'permit'
-    const permitInputs = ['address', 'address', 'uint256', 'uint256', 'uint8', 'bytes32', 'bytes32']
-    const permitSelector = utils.hexDataSlice(utils.id(`${permitName}(${permitInputs.join(',')})`), 0, 4)
+    const permitName = 'permit';
+    const permitInputs = [
+      'address',
+      'address',
+      'uint256',
+      'uint256',
+      'uint8',
+      'bytes32',
+      'bytes32',
+    ];
+    const permitSelector = utils.hexDataSlice(
+      utils.id(`${permitName}(${permitInputs.join(',')})`),
+      0,
+      4,
+    );
     const { v, r, s } = await getPermitSignature(wallet, token, {
       owner: wallet.address,
       spender: permitAndCall.address,
       value: swapAmount,
-    })
+    });
     const permitData = utils.defaultAbiCoder.encode(permitInputs, [
       wallet.address,
       permitAndCall.address,
@@ -94,22 +125,31 @@ describe('PermitAndCall', () => {
       v,
       r,
       s,
-    ])
+    ]);
 
-    const routerFunctionName = 'swapExactTokensForTokens'
-    const routerFunctionFragment = router.interface.fragments.filter(({ name }) => name === routerFunctionName)[0]
+    const routerFunctionName = 'swapExactTokensForTokens';
+    const routerFunctionFragment = router.interface.fragments.filter(
+      ({ name }) => name === routerFunctionName,
+    )[0];
     const routerFunctionSelector = utils.hexDataSlice(
-      utils.id(`${routerFunctionName}(${routerFunctionFragment.inputs.map(({ type }) => type).join(',')})`),
+      utils.id(
+        `${routerFunctionName}(${routerFunctionFragment.inputs
+          .map(({ type }) => type)
+          .join(',')})`,
+      ),
       0,
-      4
-    )
-    const routerFunctionData = utils.defaultAbiCoder.encode(routerFunctionFragment.inputs, [
-      swapAmount,
-      constants.Zero,
-      [token.address, WETH.address],
-      wallet.address,
-      constants.MaxUint256,
-    ])
+      4,
+    );
+    const routerFunctionData = utils.defaultAbiCoder.encode(
+      routerFunctionFragment.inputs,
+      [
+        swapAmount,
+        constants.Zero,
+        [token.address, WETH.address],
+        wallet.address,
+        constants.MaxUint256,
+      ],
+    );
 
     await permitAndCall.permitAndCall(
       token.address,
@@ -118,30 +158,39 @@ describe('PermitAndCall', () => {
       permitData,
       routerFunctionSelector,
       routerFunctionData,
-      { gasLimit: GAS_LIMIT }
-    )
-  })
+      { gasLimit: GAS_LIMIT },
+    );
+  });
 
   it('works with ETH as an output', async () => {
-    const permitSelector = utils.hexDataSlice(constants.HashZero, 0, 4)
-    const permitData = '0x'
+    const permitSelector = utils.hexDataSlice(constants.HashZero, 0, 4);
+    const permitData = '0x';
 
-    const routerFunctionName = 'swapExactTokensForETH'
-    const routerFunctionFragment = router.interface.fragments.filter(({ name }) => name === routerFunctionName)[0]
+    const routerFunctionName = 'swapExactTokensForETH';
+    const routerFunctionFragment = router.interface.fragments.filter(
+      ({ name }) => name === routerFunctionName,
+    )[0];
     const routerFunctionSelector = utils.hexDataSlice(
-      utils.id(`${routerFunctionName}(${routerFunctionFragment.inputs.map(({ type }) => type).join(',')})`),
+      utils.id(
+        `${routerFunctionName}(${routerFunctionFragment.inputs
+          .map(({ type }) => type)
+          .join(',')})`,
+      ),
       0,
-      4
-    )
-    const routerFunctionData = utils.defaultAbiCoder.encode(routerFunctionFragment.inputs, [
-      swapAmount,
-      constants.Zero,
-      [token.address, WETH.address],
-      wallet.address,
-      constants.MaxUint256,
-    ])
+      4,
+    );
+    const routerFunctionData = utils.defaultAbiCoder.encode(
+      routerFunctionFragment.inputs,
+      [
+        swapAmount,
+        constants.Zero,
+        [token.address, WETH.address],
+        wallet.address,
+        constants.MaxUint256,
+      ],
+    );
 
-    await token.approve(permitAndCall.address, swapAmount)
+    await token.approve(permitAndCall.address, swapAmount);
     await permitAndCall.permitAndCall(
       token.address,
       swapAmount,
@@ -149,7 +198,7 @@ describe('PermitAndCall', () => {
       permitData,
       routerFunctionSelector,
       routerFunctionData,
-      { gasLimit: GAS_LIMIT }
-    )
-  })
-})
+      { gasLimit: GAS_LIMIT },
+    );
+  });
+});
